d9a1b65327eccfb0b2b341f8f6a54b317fb28ee3
Ahammed Abdali Khan <ahammed911khan@gmail.com>
2024-11-24 12:31:22 +0600

    Create directed.cpp

---
+++ class10/directed.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int N, E;
+
+    printf("Enter the number of nodes: ");
+    scanf("%d", &N);
+
+    int Graph[N][N];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j)
+                Graph[i][j] = 0;
+            else
+                Graph[i][j] = -1;
+        }
+    }
+
+    printf("Enter the number of edges: ");
+    scanf("%d", &E);
+
+    for (int i = 0; i < E; i++) {
+        int x, y;
+        printf("Enter edge %d (format: x y): ", i + 1);
+        scanf("%d %d", &x, &y);
+
+        if (x < 0 || x >= N || y < 0 || y >= N) {
+            printf("Invalid nodes. Please try again.\n");
+            i--;
+        } else {
+            Graph[x][y] = 1;
+            /*Graph[y][x] = 1;*/
+        }
+    }
+
+    printf("\nAdjacency matrix representation of the graph:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", Graph[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
